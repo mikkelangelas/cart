ccf17242733074b0230b126c0e51424e4bc8215d
mikkelangelas <msugalski20@gmail.com>
2025-09-05 13:32:52 +0200

    :sparkles: Implement cartridge creation

--- src/cartridge.c
+++ src/cartridge.c
@@ -1,32 +1,34 @@
 #include "cartridge.h"
 
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
-static CartType get_cart_type(uint8_t code) {
-    CartType type = CART_UNKNOWN;
+#include "util.h"
+
+static CartridgeType get_cart_type(uint8_t code) {
+    CartridgeType type = CART_TYPE_UNKNOWN;
 
     switch (code) {
         case 0x00:
         case 0x08:
         case 0x09:
-            type = CART_NO_MBC;
+            type = CART_TYPE_NO_MBC;
             break;
         case 0x01:
         case 0x02:
         case 0x03:
-            type = CART_MBC1;
+            type = CART_TYPE_MBC1;
             break;
         case 0x05:
         case 0x06:
-            type = CART_MBC2;
+            type = CART_TYPE_MBC2;
             break;
         case 0x0F:
         case 0x10:
         case 0x11:
         case 0x12:
         case 0x13:
-            type = CART_MBC3;
+            type = CART_TYPE_MBC3;
             break;
     }
 
@@ -37,11 +39,43 @@ static uint32_t get_cart_ram_size(uint8_t code) {
     uint32_t size = 0;
 
     switch (code) {
-        case 0x00: size = 0; break;
-        case 0x02: size = 8192; break;
-        case 0x03: size = 32768; break;
-        case 0x04: size = 131072; break;
-        case 0x05: size = 65536; break;
+        case 0x00:
+            size = 0x00; break;
+        case 0x02:
+            size = KIB_8; break;
+        case 0x03:
+            size = KIB_32; break;
+        case 0x04:
+            size = KIB_128; break;
+        case 0x05:
+            size = KIB_64; break;
+    }
+
+    return size;
+}
+
+static uint32_t get_cart_rom_size(uint8_t code) {
+    uint32_t size = 0;
+
+    switch(code) {
+        case 0x00:
+            size = KIB_32; break;
+        case 0x01:
+            size = KIB_64; break;
+        case 0x02:
+            size = KIB_128; break;
+        case 0x03:
+            size = KIB_256; break;
+        case 0x04:
+            size = KIB_512; break;
+        case 0x05:
+            size = MIB_1; break;
+        case 0x06:
+            size = MIB_2; break;
+        case 0x07:
+            size = MIB_4; break;
+        case 0x08:
+            size = MIB_8; break;
     }
 
     return size;
@@ -50,6 +84,23 @@ static uint32_t get_cart_ram_size(uint8_t code) {
 Cartridge *create_cartridge(const char *rom_file) {
     Cartridge *new_cart = (Cartridge*)malloc(sizeof(Cartridge));
 
+    uint8_t *rom_buf = read_file_to_array(rom_file, 1);
+
+    if (rom_buf == NULL) {
+        destroy_cartridge(new_cart);
+        return NULL;
+    }
+
+    new_cart->type = get_cart_type(rom_buf[CART_TYPE_ADDR]);
+    new_cart->rom = rom_buf;
+    new_cart->rom_size = get_cart_rom_size(rom_buf[CART_ROM_SIZE_ADDR]);
+    new_cart->ram_size = (new_cart->type != CART_TYPE_MBC2)
+        ? get_cart_ram_size(rom_buf[CART_RAM_SIZE_ADDR])
+        : 512;
+
+    new_cart->ram = (uint8_t*)malloc(new_cart->ram_size);
+
+    memset(new_cart->ram, 0, new_cart->ram_size);
 
     return new_cart;
 }
@@ -66,11 +117,11 @@ uint8_t cartridge_read(Cartridge *cart, uint16_t addr) {
     uint8_t val = 0xFF;
 
     switch (cart->type) {
-        case CART_NO_MBC: break;
-        case CART_MBC1: break;
-        case CART_MBC2: break;
-        case CART_MBC3: break;
-        case CART_UNKNOWN: break;
+        case CART_TYPE_NO_MBC: break;
+        case CART_TYPE_MBC1: break;
+        case CART_TYPE_MBC2: break;
+        case CART_TYPE_MBC3: break;
+        case CART_TYPE_UNKNOWN: break;
     }
 
     return val;
@@ -78,10 +129,10 @@ uint8_t cartridge_read(Cartridge *cart, uint16_t addr) {
 
 void cartridge_write(Cartridge *cart, uint16_t addr, uint8_t val) {
     switch (cart->type) {
-        case CART_NO_MBC: break;
-        case CART_MBC1: break;
-        case CART_MBC2: break;
-        case CART_MBC3: break;
-        case CART_UNKNOWN: return;
+        case CART_TYPE_NO_MBC: break;
+        case CART_TYPE_MBC1: break;
+        case CART_TYPE_MBC2: break;
+        case CART_TYPE_MBC3: break;
+        case CART_TYPE_UNKNOWN: return;
     }
 }
--- src/cartridge.h
+++ src/cartridge.h
@@ -3,16 +3,31 @@
 
 #include <stdint.h>
 
-typedef enum CartType {
-    CART_NO_MBC,
-    CART_MBC1,
-    CART_MBC2,
-    CART_MBC3,
-    CART_UNKNOWN
-} CartType;
+#define KIB_8   0x00002000
+#define KIB_32  0x00008000
+#define KIB_64  0x00010000
+#define KIB_128 0x00020000
+#define KIB_256 0x00040000
+#define KIB_512 0x00080000
+#define MIB_1   0x00100000
+#define MIB_2   0x00200000
+#define MIB_4   0x00400000
+#define MIB_8   0x00800000
+
+#define CART_TYPE_ADDR 0x147
+#define CART_ROM_SIZE_ADDR 0x148
+#define CART_RAM_SIZE_ADDR 0x149
+
+typedef enum CartridgeType {
+    CART_TYPE_NO_MBC,
+    CART_TYPE_MBC1,
+    CART_TYPE_MBC2,
+    CART_TYPE_MBC3,
+    CART_TYPE_UNKNOWN
+} CartridgeType;
 
 typedef struct Cartridge {
-    CartType type;
+    CartridgeType type;
 
     uint8_t *rom;
     uint8_t *ram;
